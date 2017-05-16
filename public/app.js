d0c32a6935bfe8fbb829bf9691f4f8075456c40a
PJC-1 <jumpzine@gmail.com>
2017-05-15 21:18:16 -0700

    example using the util call module the .inherits() and binding/emitting an event

--- public/app.js
+++ public/app.js
@@ -1,9 +1,32 @@
 var events = require('events');
+// util module, used to inherit from objects built into node.js
+var  util = require('util');
 
-var myEmitter = new events.EventEmitter();
+// so when we create a new person we need to pass in a name
+var Person = function(name){
+    this.name = name;
+}
 
-myEmitter.on('someEvent', function(mssg){
-    console.log(mssg);
+// what I want to do is inherit the EventEmitter(), in other words I want each person to inherit the EventEmitter() so that they can bind custom events to the person object
+// first argument is who is inheriting, in this case Person
+// the second argument is what is being inherited, in our example events.EventEmitter
+util.inherits(Person, events.EventEmitter);
+
+// making three people
+var phill = new Person('phill');
+var sherry = new Person('sherry');
+var max = new Person('max');
+
+var people = [phill, sherry, max];
+
+people.forEach(function(person){
+    // we are binding the custom event 'speak' to each person
+    person.on('speak', function(mssg){
+        // this will log the name of the person who is speaking and the message passed into the call back function
+        console.log(person.name + ' said: ' + mssg);
+    });
 });
 
-myEmitter.emit('someEvent', 'the event was emitted');
+// here we are manually emitting the custom event 'speak' and passing the message 'hello world' into the call back function which will log "phill said: hello world"
+phill.emit('speak', 'hello world');
+sherry.emit('speak', 'hi dude')
