8c7709bee8a47e41e10a7e36451a17533bf43d3d
PJC-1 <jumpzine@gmail.com>
2017-05-16 21:33:47 -0700

    using readFile() asynchronyous

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
 var fs = require('fs');
 
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
+// when using the asynchronyous version of the method, a third parameter is required
+// which is the call backfunction for when it is finished reading the file. This call back function takes two arguments
+// The first being the err if there is an error reading the file and the data that we retrive.
+fs.readFile('readMe.txt', 'utf8', function(err,data){
+    console.log(data);
+});
 
-fs.writeFileSync('writeMe.txt', readMe);
+// the readFile() will now block the code, so any code after the method will run even while the file is being read.
+// we will test a log here, and the out put will log this before logging the data retrived from reading the file.
+console.log("test");
