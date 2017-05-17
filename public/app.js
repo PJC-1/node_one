8742d037e0b7dd4c1d515f4eec0395a96dcd2503
PJC-1 <jumpzine@gmail.com>
2017-05-16 21:06:16 -0700

    using the writeFileSync() method

--- public/app.js
+++ public/app.js
@@ -6,5 +6,7 @@ var fs = require('fs');
 // the first argument is the full path to the file you want to read. Second is the character encoding, working with binary and to convert that binary into something else
 var readMe = fs.readFileSync('readMe.txt', 'utf8');
 
-// remember when running this module in the terminal to be cd into the directory
-console.log(readMe);
+// the writeFilySync method is used to write a file.
+// it takes to arguments, the first being the file and path were you want to write the file. And second is the what you want to write to the file,
+// in our example we are using the contents of the readMe.txt file which is stored in a variable.
+fs.writeFileSync('writeMe.txt', readMe);
