85892e615d07603bfbe7e0f58a6aaea8fef60140
PJC-1 <jumpzine@gmail.com>
2017-05-17 21:33:05 -0700

    file system unlink() exmaple

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('readMe.txt', 'utf8', function(err,data){
-    fs.writeFile('writeMe.txt', data);
-});
+// the unlink() method is used to delete files, it takes an argument which is the file name/path
+// beware that if you call this method again, and the file does not exists. it will throw an error. in this case it would error: no such file or directory.
+
+fs.unlink('writeMe.txt');
