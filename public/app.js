1ed38687cd0d899077b7e8a3ca1c35e1b0f5e38f
PJC-1 <jumpzine@gmail.com>
2017-07-04 18:17:55 -0700

    created a write stream

--- public/app.js
+++ public/app.js
@@ -2,10 +2,12 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
 
 myReadStream.on('data', function(chunk){
     console.log('new chunch received:');
-    console.log(chunk);
+    myWriteStream.write(chunk);
 });
 
 
