b8f165e75a02c43b90138db78b24ead2ea6e312f
PJC-1 <jumpzine@gmail.com>
2017-07-15 16:54:11 -0700

    added a route that uses the route parameter to facilitate dynamic routing

--- public/app.js
+++ public/app.js
@@ -10,8 +10,8 @@ app.get('/contact', function(req, res){
     res.send('this is the contact page');
 });
 
-app.get('/profile/:id', function(req,res){
-    res.send('You requested to see a profile with the id of ' + req.params.id);
+app.get('/profile/:name', function(req,res){
+    res.send('You requested to see a profile with the name of ' + req.params.name);
 });
 
 app.listen(3000);
