00aec6daf84267114fa7f0fea6bff8c7c7561906
PJC-1 <jumpzine@gmail.com>
2017-07-16 14:42:02 -0700

    made use of the render method to serve an ejs view and also used some dynamic content with the url parameter

--- public/app.js
+++ public/app.js
@@ -2,16 +2,18 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('this is the homepage.');
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.get('/contact', function(req, res){
-    res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', function(req,res){
-    res.send('You requested to see a profile with the name of ' + req.params.name);
+    res.render('profile', {person: req.params.name});
 });
 
 app.listen(3000);
